c6755f6d5c359e0578cfeb627b0c4a9116a079e6
machy <machy85@gmail.com>
2017-01-16 20:14:07 -0800

    add tests.py and .travis.yml

--- scripts/list_files.py
+++ scripts/list_files.py
@@ -1,21 +1,25 @@
 ﻿import os
 import argparse
 
+def list_files(input_path, partial_name=None, output_file='output.txt'):
+    fout = open(output_file, "w")
+    for path, subdirs, files in os.walk(input_path):
+        for file_name in files:
+            if partial_name and partial_name not in file_name:
+                continue
+            f = os.path.join(input_path, file_name)
+            print(file_name)
+            fout.write(str(f) + "\n")
+
 if __name__ == "__main__" :
     parser = argparse.ArgumentParser(description='List the files in a folder.')
-    parser.add_argument('folder_path', type=str, help='Path to the folder')
+    parser.add_argument('input_path', type=str, help='Path to the input folder')
     parser.add_argument('--output_file', type=str, default='output.txt', help='Output file')
     parser.add_argument('--partial_name', type=str, default=None, help='Part of file name')
 
     args = parser.parse_args()
-    path = args.folder_path
-    part = args.partial_name
+    input_path = args.input_path
+    partial_name = args.partial_name
+    output_file = args.output_file
 
-    fout = open(args.output_file, "w")
-    for path, subdirs, files in os.walk(path):
-        for file_name in files:
-            if part and part not in file_name:
-                continue
-            f = os.path.join(path, file_name)
-            print(file_name)
-            fout.write(str(f) + "\n")
+    list_files(input_path, partial_name, output_file)
---
+++ tests.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.insert(0, './scripts')
+
+import list_files
+
+if __name__ == '__main__':
+    list_files.list_files("./scripts")
