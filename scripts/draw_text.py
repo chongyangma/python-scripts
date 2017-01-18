ea06f7353b5b36b2d1ba199402225fc87267c75d
machy <machy85@gmail.com>
2017-01-17 20:45:12 -0800

    update draw_text.py and add related test

--- scripts/draw_text.py
+++ scripts/draw_text.py
@@ -10,10 +10,38 @@ def is_image(file_name):
     else:
         return False
 
+def draw_text(input_path, output_path, text=None, font_size=20,
+ pos_x=5, pos_y=5, color_r=255, color_g=255, color_b=255):
+    fnt = ImageFont.truetype("Arial.ttf", font_size)
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    count = 0
+    for path, subdirs, files in os.walk(input_path):
+        for filename in files:
+            f = os.path.join(path, filename)
+            if not is_image(f):
+                continue
+            img = Image.open(f)
+            img = img.convert("RGBA")
+            text_to_draw = filename
+            if text:
+                text_to_draw = text
+            draw = ImageDraw.Draw(img)
+            draw.text((pos_x, pos_y), text_to_draw, font=fnt, fill=(color_r, color_g, color_b, 255))
+            newfilename = os.path.join(output_path, filename)
+            img.save(newfilename)
+            count = count + 1
+
+    return count
+
+
 if __name__ == "__main__" :
     parser = argparse.ArgumentParser(description='Draw test over the files in a folder.')
     parser.add_argument('input_path', type=str, help='Path to the input folder')
-    parser.add_argument('--output_folder', type=str, default=None, help='Output folder name')
+    parser.add_argument('output_path', type=str, help='Path to the output folder')
+    parser.add_argument('--text', type=str, default=None, help='Text to draw')
     parser.add_argument('--font_size', type=int, default=20, help='Font size')
     parser.add_argument('--pos_x', type=int, default=5, help='Start position X')
     parser.add_argument('--pos_y', type=int, default=5, help='Start position Y')
@@ -23,7 +51,8 @@ if __name__ == "__main__" :
 
     args = parser.parse_args()
     input_path = args.input_path
-    output_folder = args.output_folder
+    output_path = args.output_path
+    text = args.text
     font_size = args.font_size
     pos_x = args.pos_x
     pos_y = args.pos_y
@@ -31,25 +60,4 @@ if __name__ == "__main__" :
     color_g = args.color_g
     color_b = args.color_b
 
-    fnt = ImageFont.truetype("Arial.ttf", font_size)
-
-    if output_folder:
-        output_path = os.path.join(input_path, output_folder)
-    else:
-        output_path = os.path.join(input_path, "new")
-    if not os.path.exists(output_path):
-        os.mkdir(output_path)
-
-    count = 1;
-    for path, subdirs, files in os.walk(input_path):
-        for filename in files:
-            f = os.path.join(path, filename)
-            if not is_image(f):
-                continue
-            img = Image.open(f)
-            img = img.convert("RGBA")
-            draw = ImageDraw.Draw(img)
-            draw.text((pos_x, pos_y), filename, font=fnt, fill=(color_r, color_g, color_b, 255))
-            newfilename = os.path.join(output_path, filename)
-            img.save(newfilename)
-            count = count + 1
+    draw_text(input_path, output_path, text, font_size, pos_x, pos_y, color_r, color_g, color_b)
--- tests.py
+++ tests.py
@@ -2,7 +2,11 @@ import sys
 sys.path.insert(0, './scripts')
 
 import list_files
+import draw_text
 
 if __name__ == '__main__':
-    file_count = list_files.list_files("./scripts")
-    print("Have listed %d files." % file_count)
+    test_data_path = "./data"
+    list_file_count = list_files.list_files(test_data_path)
+    print("Have listed %d files." % list_file_count)
+    draw_file_count = draw_text.draw_text(test_data_path, './output')
+    print("Have drawn %d images." % draw_file_count)
